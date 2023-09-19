6d1237b2402c4753b209a00d9e1ed4c8be4deca2
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-09-19 09:47:51 +0600

    added validation

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -1,12 +1,30 @@
 import express from 'express';
 import { AcademicSemesterController } from './academicSemester.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { academicSemesterValidation } from './academicSemester.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
 router.get('/', AcademicSemesterController.getAllFromDb);
 router.get('/:id', AcademicSemesterController.getSingleSemester);
-router.patch('/:id', AcademicSemesterController.updateFromDb);
-router.delete('/:id', AcademicSemesterController.deleteFromDb);
-router.post('/', AcademicSemesterController.insertIntoDb);
+router.patch(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  validateRequest(academicSemesterValidation.updateSchema),
+  AcademicSemesterController.updateFromDb
+);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  AcademicSemesterController.deleteFromDb
+);
+router.post(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  validateRequest(academicSemesterValidation.createSchema),
+  AcademicSemesterController.insertIntoDb
+);
 
 export const AcademicSemesterRoutes = router;
--- src/app/modules/academicSemester/academicSemester.validation.ts
+++ src/app/modules/academicSemester/academicSemester.validation.ts
@@ -1,20 +1,25 @@
 import { z } from 'zod';
+import {
+  academicSemesterCodes,
+  academicSemesterMonths,
+  academicSemesterTitles
+} from './academicSemester.constant';
 
 const createSchema = z.object({
   body: z.object({
     year: z.number({
       required_error: 'year is required'
     }),
-    title: z.string({
+    title: z.enum([...academicSemesterTitles] as [string, ...string[]], {
       required_error: 'title is required'
     }),
-    code: z.string({
+    code: z.enum([...academicSemesterCodes] as [string, ...string[]], {
       required_error: 'code is required'
     }),
-    startMonth: z.string({
+    startMonth: z.enum([...academicSemesterMonths] as [string, ...string[]], {
       required_error: 'startMonth is required'
     }),
-    endMonth: z.string({
+    endMonth: z.enum([...academicSemesterMonths] as [string, ...string[]], {
       required_error: 'endMonth is required'
     })
   })
@@ -24,16 +29,16 @@ const updateSchema = z.object({
     year: z.number({
       required_error: 'year is required'
     }),
-    title: z.string({
+    title: z.enum([...academicSemesterTitles] as [string, ...string[]], {
       required_error: 'title is required'
     }),
-    code: z.string({
+    code: z.enum([...academicSemesterCodes] as [string, ...string[]], {
       required_error: 'code is required'
     }),
-    startMonth: z.string({
+    startMonth: z.enum([...academicSemesterMonths] as [string, ...string[]], {
       required_error: 'startMonth is required'
     }),
-    endMonth: z.string({
+    endMonth: z.enum([...academicSemesterMonths] as [string, ...string[]], {
       required_error: 'endMonth is required'
     })
   })

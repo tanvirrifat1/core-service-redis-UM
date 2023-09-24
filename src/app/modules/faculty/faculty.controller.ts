bb83afd5978361a5924ca66db35e2a42173813ec
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-09-24 14:05:53 +0600

    aded more

--- src/app/modules/faculty/faculty.controller.ts
+++ src/app/modules/faculty/faculty.controller.ts
@@ -2,9 +2,63 @@ import { NextFunction, Request, Response } from 'express';
 import { FacultyService } from './faculty.service';
 import sendResponse from '../../../shared/response';
 
-const updateIntoDB = async (req: Request, res: Response, next: NextFunction) => {
+const getAllFromDB = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const result = await FacultyService.updateIntoDB(req);
+    const result = await FacultyService.getAllFromDB(req);
+    sendResponse(res, result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getMyCourses = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await FacultyService.getMyCourses(req);
+    sendResponse(res, result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getMyCourseStudents = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await FacultyService.getMyCourseStudents(req);
+    sendResponse(res, result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getByIdFromDB = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await FacultyService.getByIdFromDB(req);
+    sendResponse(res, result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getFacultyProfile = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await FacultyService.getFacultyProfile(req);
+    sendResponse(res, result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateOneInDB = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await FacultyService.updateOneInDB(req);
+    sendResponse(res, result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteByIdFromDB = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await FacultyService.deleteByIdFromDB(req);
     sendResponse(res, result);
   } catch (error) {
     next(error);
@@ -12,5 +66,11 @@ const updateIntoDB = async (req: Request, res: Response, next: NextFunction) => 
 };
 
 export const FacultyController = {
-  updateIntoDB
+  updateOneInDB,
+  getAllFromDB,
+  getByIdFromDB,
+  getFacultyProfile,
+  getMyCourseStudents,
+  getMyCourses,
+  deleteByIdFromDB
 };
